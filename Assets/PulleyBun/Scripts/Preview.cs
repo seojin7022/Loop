9c48cadb93932009beab8c14da812950c17bff45
PulleyBun <z22x22c22@naver.com>
2026-08-08 00:06:37 +0900

    프리뷰 표시

---
+++ Assets/PulleyBun/Scripts/Preview.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace PulleyBun
+{
+    public class Preview : MonoBehaviour
+    {
+        [SerializeField] float predictDepth = 60f;
+        [SerializeField] float predictLength = 100f;
+        [SerializeField] LineRenderer lineRenderer;
+
+        void Update()
+        {
+            lineRenderer.SetPosition(0, transform.position);
+            var position = transform.position;
+            var direction = transform.right;
+            for (int i = 1; i < predictDepth; i++)
+            {
+                lineRenderer.positionCount = i + 1;
+                var hit = Physics2D.Raycast(position, direction, predictLength);
+                if (hit)
+                {
+                    lineRenderer.SetPosition(i, hit.point);
+                    direction = Vector2.Reflect(direction, hit.normal);
+                    //Debug.Log($"direction: {direction}, hit.transform.up: {hit.transform.up}, hit.normal: {hit.normal}");
+                    position = hit.point + (Vector2)direction * 0.01f;
+                }
+                else
+                {
+                    lineRenderer.SetPosition(i, position + direction * predictLength);
+                    break;
+                }
+            }
+        }
+    }
+}
