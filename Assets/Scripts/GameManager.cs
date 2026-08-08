013dcb0bfcba52aad993f731cd01f0a2a95ad4fa
rrip1234 <ljm3290@naver.com>
2026-08-09 00:10:49 +0900

    add playback

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,5 +9,30 @@ public class GameManager : MonoBehaviour
     public void WaveCleared()
     {
         waveManager.FinishWave();
+    }
+
+    public InputAction playback;
+    public float holdThrehold, playbackSpeed;
+
+    float holdTime;
+
+    void Awake()
+    {
+        playback.Enable();
+    }
+
+    void Update()
+    {
+        if(playback.IsPressed())
+        {
+            holdTime += Time.unscaledDeltaTime;
+        }
+        else
+            holdTime = 0;
+        
+        if(holdTime > holdThrehold)
+            Time.timeScale = playbackSpeed;
+        else
+            Time.timeScale = 1.0f;
     }
 }
